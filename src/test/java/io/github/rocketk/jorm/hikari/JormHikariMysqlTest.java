1a27223efc373518cf97ee46a596a7ada0a760f1
pengyu <pengyufreedom@163.com>
2022-08-11 10:37:29 +0800

    disable test cases for mysql

--- src/test/java/io/github/rocketk/jorm/hikari/JormHikariMysqlTest.java
+++ src/test/java/io/github/rocketk/jorm/hikari/JormHikariMysqlTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author pengyu
  *
  */
-//@Disabled
+@Disabled
 public class JormHikariMysqlTest extends CrudCasesTest {
 
     @Override
